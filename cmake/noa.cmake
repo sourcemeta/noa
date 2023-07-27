fc21c83133a1ac05df2db0a98fc8cddd30827a20
Juan Cruz Viotti <jv@jviotti.com>
2023-07-27 14:09:58 -0400

    Hide shared library symbols by default
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/noa.cmake
+++ cmake/noa.cmake
@@ -1,4 +1,5 @@
 set(NOA_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/noa")
 include("${NOA_DIRECTORY}/shim.cmake")
+include("${NOA_DIRECTORY}/defaults.cmake")
 include("${NOA_DIRECTORY}/commands/copy-file.cmake")
 include("${NOA_DIRECTORY}/targets/clang-format.cmake")

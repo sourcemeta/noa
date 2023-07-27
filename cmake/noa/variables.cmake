46cee6217b963101d68eee1f3a389c94a4e8ac8b
Juan Cruz Viotti <jv@jviotti.com>
2023-07-27 14:16:00 -0400

    Add TODO item in compiler variables
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/noa/variables.cmake
+++ cmake/noa/variables.cmake
@@ -1,4 +1,5 @@
 # Compiler detection (C++)
+# TODO: Detect compilers on programming languages other than C++
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
   set(NOA_COMPILER_LLVM ON)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")

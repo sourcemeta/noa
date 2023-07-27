035ce81f9e9d6eced83659e3de6ae120a3e19a75
Juan Cruz Viotti <jv@jviotti.com>
2023-07-27 14:39:01 -0400

    Cleanup symbol visibility
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/noa/defaults.cmake
+++ cmake/noa/defaults.cmake
@@ -1,9 +1,11 @@
 # Hide symbols from shared libraries by default
-set(CMAKE_CXX_VISIBILITY_PRESET hidden)
 set(CMAKE_VISIBILITY_INLINES_HIDDEN YES)
-
 if("CXX" IN_LIST NOA_LANGUAGES)
-  # By default, stay within ISO C++
+  set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+endif()
+
+# By default, stay within ISO C++
+if("CXX" IN_LIST NOA_LANGUAGES)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   set(CMAKE_CXX_EXTENSIONS OFF)
 endif()
